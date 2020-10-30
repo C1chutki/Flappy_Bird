0852142b213583a186cbef328577bc2e6a538fb9
Kamil <42701679+C1chutki@users.noreply.github.com>
2020-10-30 18:13:42 +0100

    Basic

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("GameHandler.Start");
+
+        int count = 0;
+        FunctionPeriodic.Create(() =>
+        {
+            CMDebug.TextPopupMouse("Ding!" + count);
+            count++;
+        }, .300f);
+    }
+}
