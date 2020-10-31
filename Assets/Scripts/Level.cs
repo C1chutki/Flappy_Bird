1e8b33013173a30ad00c064fe25f3562605fcaa4
Kamil <42701679+C1chutki@users.noreply.github.com>
2020-10-31 01:51:53 +0100

    Pipes and Ui

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,28 +7,143 @@ public class Level : MonoBehaviour
 
     private const float CAMERA_ORTHO_SIZE = 50f;
     private const float PIPE_WIDTH = 8f;
-    private const float PIPE_HEAD_HEIGHT= 3f;
+    private const float PIPE_HEAD_HEIGHT = 3f;
+    private const float PIPE_MOVE_SPEED = 30f;
+    private const float PIPE_DESTROY_X_POSITION = -125f;
+    private const float PIPE_SPAWN_X_POSITION = +110f;
+    private const float BIRD_X_POSITION = 0f;
+
+    private static Level instance;
+    public static Level GetInstance()
+    {
+        return instance;
+    }
+
+    private List<Pipe> pipeList;
+    private int pipesPassedCount;
+    private int pipesSpawned;
+    private float pipeSpawnTimer;
+    private float pipeSpawnTimerMax;
+    private float gapSize;
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard,
+        Impossible,
+    }
+
+    private void Awake()
+    {
+        instance = this;
+        pipeList = new List<Pipe>();
+        pipeSpawnTimerMax = 1f;
+        SetDifficulty(Difficulty.Easy);
+    }
+
+
     private void Start()
     {
-        CreatePipe(50f,0f, true);
-        CreatePipe(50f,0f, false);
+        //CreatePipe(50f,0f, true);
+        //CreatePipe(50f,0f, false);
+        //CreateGapPipes(40f, 30f, 20f);
     }
+
+    private void Update()
+    {
+        HandlePipeMovement();
+        HandlePipeSpawning();
+    }
+    private void HandlePipeSpawning()
+    {
+        pipeSpawnTimer -= Time.deltaTime;
+        if (pipeSpawnTimer < 0)
+        {
+            //Time to spawn another pipe
+            pipeSpawnTimer += pipeSpawnTimerMax;
+
+
+            float heightEdgeLimit = 10f;
+            float totalHeight = CAMERA_ORTHO_SIZE * 2f;
+
+            float minHeight = gapSize * .5f + heightEdgeLimit;
+            float maxHeight = totalHeight - gapSize * .5f -heightEdgeLimit;
+
+            float height = Random.Range(minHeight, maxHeight);
+            
+            
+            CreateGapPipes(height, gapSize, PIPE_SPAWN_X_POSITION);
+        }
+    }
+
+    private void HandlePipeMovement()
+    {
+        for (int i = 0; i < pipeList.Count; i++)
+        {
+            Pipe pipe = pipeList[i];
+            bool isToTheRightOfBird = pipe.GetXPosition() > BIRD_X_POSITION;
+            pipe.Move();
+            if (isToTheRightOfBird && pipe.GetXPosition() <= BIRD_X_POSITION)
+            {
+                //pipes passed bird
+                pipesPassedCount++;
+            }
+            if (pipe.GetXPosition() < PIPE_DESTROY_X_POSITION)
+            {
+                //Destroy Pipe
+                pipe.DestroySelf();
+                pipeList.Remove(pipe);
+                i--;
+            }
+        }  
+    }
+
+
+    private void SetDifficulty(Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                gapSize = 50f;
+                pipeSpawnTimerMax = 1.2f;
+                break;
+                
+            case Difficulty.Medium:
+                gapSize = 40f;
+                pipeSpawnTimerMax = 1.1f;
+                break;
+                
+            case Difficulty.Hard:
+                gapSize = 33f;
+                pipeSpawnTimerMax = 1f;
+                break;
+                
+            case Difficulty.Impossible:
+                gapSize = 24f;
+                pipeSpawnTimerMax = 0.9f;
+                break;
+
+        }
+    }
+
+    private Difficulty GetDifficulty()
+    {
+        if (pipesSpawned >= 30) return Difficulty.Impossible;
+        if (pipesSpawned >= 20) return Difficulty.Hard;
+        if (pipesSpawned >= 10) return Difficulty.Medium;
+        return Difficulty.Easy;
+
+    }
+    
 
     private void CreateGapPipes(float gapY, float gapSize, float xPosition)
     {
-        
+        CreatePipe(gapY - gapSize * .5f, xPosition, true);
+        CreatePipe(CAMERA_ORTHO_SIZE * 2f - gapY - gapSize * .5f, xPosition, false);
+        pipesSpawned++;
+        SetDifficulty(GetDifficulty());
     }
-
-
-
-
-
-
-
-
-
-
-
 
     //Creat pipe
     private void CreatePipe(float height, float xPosition, bool createBottom)
@@ -46,11 +161,6 @@ public class Level : MonoBehaviour
             pipeHeadYPosition = +CAMERA_ORTHO_SIZE - height + PIPE_HEAD_HEIGHT * .5f;
         }
         pipeHead.position = new Vector3(xPosition, pipeHeadYPosition);
-
-
-
-
-
 
         // set up pipe body
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
@@ -72,6 +182,49 @@ public class Level : MonoBehaviour
         BoxCollider2D pipeBodyBoxCollider = pipeBody.GetComponent<BoxCollider2D>();
         pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
         pipeBodyBoxCollider.offset = new Vector2(0f, height * .5f);
+
+        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        pipeList.Add(pipe);
              
+    }
+
+    public int GetPipesSpawned()
+    {
+        return pipesSpawned;
+    }
+    public int GetPipesPassedCount()
+    {
+        return pipesPassedCount/2;
+    }
+
+
+
+    // Represents a single pipe
+    private class Pipe {
+        private Transform pipeHeadTransform;
+        private Transform pipeBodyTransform;
+        
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform)
+        {
+            this.pipeHeadTransform = pipeHeadTransform;
+            this.pipeBodyTransform = pipeBodyTransform;
+        }
+
+        public void Move()
+        {
+            pipeHeadTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+        }
+
+        public float GetXPosition()
+        {
+            return pipeHeadTransform.position.x;
+        }
+
+        public void DestroySelf()
+        {
+            Destroy(pipeHeadTransform.gameObject);
+            Destroy(pipeBodyTransform.gameObject);
+        }
     }
 }
